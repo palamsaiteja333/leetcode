c4cc4aa79408cd0bd820ff6bbd25e56ff977c443
Sai Teja <51771508+palamsaiteja333@users.noreply.github.com>
2025-01-25 16:32:25 -0400

    [1] [Time Beats: 98.91%] [Memory Beats: 29.54%] - LeetPush

--- Arrays/1-Two-Sum.java
+++ Arrays/1-Two-Sum.java
@@ -2,17 +2,30 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         // bruteforce solution would be taking 2 loops
         // where looping each integer with other integers to compare with the target
-        for(int i=0; i<nums.length; i++){
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i, j};
-                }
+        // TC: O(n*n)
+        // SC: O(1)
+        // for(int i=0; i<nums.length; i++){
+        // for(int j=i+1; j<nums.length; j++){
+        // if(nums[i] + nums[j] == target){
+        // return new int[]{i, j};
+        // }
+        // }
+        // }
+
+        // return new int[]{};
+
+        // Optimal Solution
+        // TC: O(n)
+        Map<Integer, Integer> map = new HashMap<>();
+        int diff;
+        for (int i = 0; i < nums.length; i++) {
+            diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[] { i, map.get(diff) };
             }
+            map.put(nums[i], i);
         }
+        return new int[] {};
 
-        return new int[]{};
-        // Map<Integer, Integer> map = new HashMap<>();
-
-        
     }
 }
